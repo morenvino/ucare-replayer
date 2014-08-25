370d48bb0fedd7656781d7295057fec96459d979
morenvino <morenvino@ucare-05.cs.uchicago.edu>
2014-08-25 15:24:45 -0500

    minor update

--- filter-trace.py
+++ filter-trace.py
@@ -5,8 +5,8 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="trace file to process")
-parser.add_argument("-s", type=int, help="start filter from this second")
-parser.add_argument("-f", type=int, help="finish filter until this second")
+parser.add_argument("-s", help="start filter from this second", type=int, default=0)
+parser.add_argument("-f", help="finish filter until this second", type=int, default=0)
 args = parser.parse_args()
 
 start = args.s * 1000 # convert to ms
@@ -15,8 +15,8 @@ with open(args.file) as f:
 	for line in f:
 		token = line.split(" ")
 		time = float(token[0].strip())
-		if time <= start:
+		if time < start:
 			continue
-		if time >= finish:
+		if time > finish:
 			break
 		sys.stdout.write(line)
